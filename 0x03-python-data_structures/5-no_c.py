d77f3b1c0d6905f8ce77ecd4a5bdd69dce768c42
Muhammed Obari <101449678+WeebLordMonsuno@users.noreply.github.com>
2022-07-05 23:58:07 +0100

    py funct

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
-assdfgpp
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    """
+    A function to remove all characters c and C from a string.
+    """
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
